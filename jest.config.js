6dca12c98f1ddd10868f13ff1f819e20b8c6034f
hubo36 <hubo36@jd.com>
2020-05-06 00:10:46 +0800

    chore: jest

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,21 @@
 const {defaults} = require('jest-config');
+const tsConfig = require('./tsconfig.json');
 
+tsConfig.compilerOptions.types = ["jest"];
 module.exports = {
     globals: {
         'ts-jest': {
-          tsConfig: 'tsconfig.test.json'
+          tsConfig: tsConfig.compilerOptions
         }
     },
     moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts'],
     moduleNameMapper: {
-        "^APP/(.*)$": "<rootDir>/app/$1",
+        "^ROOT/(.*)$": "<rootDir>/src/$1",
     },
     setupFiles: ['<rootDir>/jest.setup.js'], // 运行测试前可执行的脚本（比如注册enzyme的兼容）
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
-        '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+        '^.+\\.(js|mjs)$': '<rootDir>/node_modules/babel-jest',
     },
     testRegex: '^.+\\.test\\.(js|ts)$',
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'], //转换时需忽略的文件
