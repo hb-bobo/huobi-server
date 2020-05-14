98ac060fdbde2d8e68df7429b443950e6ff251d6
hubo36 <hubo36@jd.com>
2020-05-14 08:24:24 +0800

    chore: change port

--- src/common/getInterval.ts
+++ src/common/getInterval.ts
@@ -2,11 +2,18 @@ import moment from 'moment';
 
 /**
  * 获取一段时间
+ * @param {string} time 24h
  * @return {Date[]}
  */
-export function getInterval() {
+export function getInterval(timeDesription: string) {
+    const time = timeDesription.match(/\d+/) || 24;
+    let factor = 60 * 60 * 1000;
+    if(timeDesription.includes('h')) {
+        factor = 60 * 60 * 1000;
+    }
     return [
-        moment(Date.now() - (24 * 60 * 60 * 1000)).format("YYYY/MM/DD H:mm:ss"),
+        moment(Date.now() - (time * factor)).format("YYYY/MM/DD H:mm:ss"),
         moment().format("YYYY/MM/DD H:mm:ss")
     ]
 }
+
