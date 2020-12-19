6f0105b304b123583e9bf10510ecc83ca75f0cb6
hubo36 <hubo36@jd.com>
2020-12-20 00:50:51 +0800

    chore: Auto push after build

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    port: 3001,
+    port: 3003,
     publicPath: path.join(__dirname, '../public'),
     huobi: {
         ws_url_prex: 'wss://api.huobi.de.com/ws', // "wss://www.hbdm.com/ws", 'wss://api.huobi.pro/ws
