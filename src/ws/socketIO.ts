f2735e616eb3e5c76974002d9ff3044bcc9b5003
hubo36 <hubo11@jd.com>
2021-01-03 14:29:24 +0800

    feat: handle ws subsription from  clinet

--- src/ws/socketIO.ts
+++ src/ws/socketIO.ts
@@ -3,6 +3,7 @@ import { outLogger } from 'ROOT/common/logger';
 import { ws_event } from 'ROOT/huobi/ws/events';
 import { ws } from 'ROOT/huobi/ws/ws'
 import { WS_SUB } from 'ROOT/huobi/ws/ws.cmd';
+
 /**
  * 与客户端的socket
  */
@@ -10,9 +11,12 @@ export const socketIO = socket({
     path: '/socket.io',
     serveClient: false,
 });
+const sockets: Record<string, socket.Socket> = {}
 socketIO.on('connection', function (socket) {
     let unSub = () => {}
+    sockets[socket.id] = socket;
     socket.on('sub', function ({symbol}) {
+        symbol = symbol.toLowerCase();
         ws.sub(WS_SUB.kline(symbol, '1min'), socket.id);
         ws.sub(WS_SUB.depth(symbol), socket.id);
         ws.sub(WS_SUB.tradeDetail(symbol), socket.id);
@@ -24,6 +28,7 @@ socketIO.on('connection', function (socket) {
     })
     socket.on("disconnect", (reason) => {
         unSub();
+        delete sockets[socket.id];
     });
     // 发送单个
     // socket.send( 'sss')
@@ -31,4 +36,21 @@ socketIO.on('connection', function (socket) {
     // socketIO.sockets.emit('event', 'xxx')
     outLogger.info(`socket connected: ${socket.id}`);
 });
-// socketIO.on('')
+ws_event.on("server:ws:message", function(data) {
+    if (data.data && data.data.symbol) {
+        for (const key in ws.cache) {
+            if (!Object.prototype.hasOwnProperty.call(ws.cache, key)) {
+                return;
+            }
+            const ids = ws.cache[key];
+            ids.forEach((id) => {
+                if (!sockets[id]) {
+                    return;
+                }
+                sockets[id].send(data);
+            })
+        }
+    }
+    // socketIO.sockets.send(data);
+});
+
