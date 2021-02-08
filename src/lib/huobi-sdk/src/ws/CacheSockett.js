3157fc6918c9e320afa63a9f2c8ffcbb723b2800
hubo36 <hubo11@jd.com>
2021-02-08 17:07:33 +0800

    chore: Auto push after build

--- src/lib/huobi-sdk/src/ws/CacheSockett.js
+++ src/lib/huobi-sdk/src/ws/CacheSockett.js
@@ -18,8 +18,11 @@ class CacheSockett {
         // ws.close();
         ws.open();
         ws.on('open', () => {
+            const list = Object.keys(this.cache);
+            list.forEach((str) => {
+                this.ws.send(str.replace('sub', 'unsub'));
+            });
             setTimeout(() => {
-                const list = Object.keys(this.cache);
                 list.forEach((str) => {
                     this.ws.send(str);
                 });
