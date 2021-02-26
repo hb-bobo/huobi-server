1b74a9f221576450b3e7525c3a1049ad25f13f0e
hubo36 <hubo11@jd.com>
2021-02-26 18:48:54 +0800

    chore: 下载历史数据

--- src/lib/quant/Quant.tran.ts
+++ src/lib/quant/Quant.tran.ts
@@ -13,7 +13,7 @@ import got from 'got/dist/source';
 const writeFilePromisify = promisify(writeFile);
 const readFilePromisify = promisify(readFile);
 const publicPath = config.get<string>('publicPath');
-const fileName = 'htusdt-5min-2021-02-21'
+const fileName = 'btcusdt-5min-2021-02-26'
 const jsonFilePath = join(publicPath, `/download/history-data/${fileName}.json`);
 
 async function download() {
