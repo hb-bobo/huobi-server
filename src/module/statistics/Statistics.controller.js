d6d3adce3a811eed2ae61f2d8abb3db5aa70a3ad
hubo36 <hubo11@jd.com>
2021-03-02 15:17:52 +0800

    chore: Auto push after build

--- src/module/statistics/Statistics.controller.js
+++ src/module/statistics/Statistics.controller.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const start_1 = require("../../huobi/start");
+const dayjs_1 = __importDefault(require("dayjs"));
 class StatisticsController {
 }
 exports.default = StatisticsController;
@@ -15,8 +19,14 @@ StatisticsController.index = async (ctx) => {
         const symbols = Object.keys(start_1.trader.orderConfigMap);
         const symbolsData = {};
         symbols.forEach(symbol => {
-            console.log(start_1.trader.orderConfigMap[symbol].quant.analyser.result);
-            symbolsData[symbol] = start_1.trader.orderConfigMap[symbol].quant.analyser.result;
+            const { result } = start_1.trader.orderConfigMap[symbol].quant.analyser;
+            symbolsData[symbol] = result.map((item) => {
+                const time = dayjs_1.default(item.time).utcOffset(8).format('YYYY-MM-DD H:mm:ss');
+                return {
+                    ...item,
+                    time
+                };
+            });
         });
         ctx.sendSuccess({
             data: symbolsData
